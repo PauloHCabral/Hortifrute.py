83890790913f6ce87cf463d0c124150882954b03
PauloHCabral <61445141+PauloHCabral@users.noreply.github.com>
2023-12-18 22:50:01 -0300

    Add files via upload

---
+++ Hortifrute.py
@@ -0,0 +1,27 @@
+
+print('Bem-Vindo Ao Hortifruti Deliviry')
+print('Já é nosso cliente ?')
+cliente = input()
+
+
+
+while cliente =='Não':
+    print(f'Faça o seu cadastro')
+    nome=(input('Informe seu nome:'))
+    idade=(input('Informe sua idade:'))
+    break
+if cliente =='Sim':
+    print('Boas Compras')
+
+
+
+frutas = {1:'Banana',2:'Uva',3:'Maça',4:'Maracuja',5:'Goiaba',6:'Melancia',7:'Manga',8:'Limão',9:'Melão',10:'Pera'}
+
+produtos = print('Confira os nossos produtos:')
+print(frutas)
+
+
+
+
+
+
